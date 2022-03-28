bdd18d69e22f0e6fbcb4681690ad9fcfb4a4fc8c
David Fernández Flores <56683644+DavidFFDAW@users.noreply.github.com>
2022-03-28 11:25:23 +0200

    icons

--- frontend/src/components/Boards/NewCricketBoard.jsx
+++ frontend/src/components/Boards/NewCricketBoard.jsx
@@ -1,4 +1,16 @@
+import { FaSlash, FaSkullCrossbones } from 'react-icons/fa';
+import { ImCross } from 'react-icons/im';
+
+
 export default function NewCricketBoard ({ scoreboard, addPoints, cricketService }) {
+
+    const parser = {
+      ' ': ' ',
+      '/': <FaSlash/>,
+      'X': <ImCross/>,
+      '⦻': <FaSkullCrossbones/>
+    }
+
     return (
         <div className="down cricket-board">
             <table>
@@ -12,13 +24,13 @@ export default function NewCricketBoard ({ scoreboard, addPoints, cricketService
                     cricketService.getBoardWithScoresInMiddle(scoreboard).map((item,ind) => {
                         return (
                             <tr key={ind}>
-                                { item.st.map((it,id) => (<td key={id}>{ it }</td> )) }
+                                { item.st.map((it,id) => (<td key={id}>{ parser[it] }</td> )) }
                                 <td>
                                     <button onClick={ _ => addPoints(item.pt) } className="pt-btn">{ item.pt }</button>
                                     <button onClick={ _ => addPoints(item.pt, 2) } className="pt-btn double">D{ item.pt }</button>
                                     { item.pt !== '25' && <button onClick={ _ => addPoints(item.pt,3) } className="pt-btn triple">T{ item.pt }</button> }
                                 </td>
-                                { item.nd.map((el,ix) => (<td key={ ix }>{ el }</td>)) }
+                                { item.nd.map((el,ix) => (<td key={ ix }>{ parser[el] }</td>)) }
                             </tr>
                         );
                     })
