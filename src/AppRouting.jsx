c92a5a110d232c9daca4084b9ce63a2a0996b074
DavidFFDAW <davidferflo2@gmail.com>
2022-11-26 19:50:04 +0100

    feat(config): prettier and routes

--- src/AppRouting.jsx
+++ src/AppRouting.jsx
@@ -1,24 +1,27 @@
-import { lazy, Suspense } from "react";
-import { BrowserRouter, Route } from "react-router-dom";
-import RoutesWithNotFound from "./components/routes/RouteWithNotfound";
-import { Spinner } from "./components/spinner/Spinner";
+import { DartsContextProvider } from 'context/DartsContext';
+import { lazy, Suspense } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import RoutesWithNotFound from './components/routes/RouteWithNotfound';
+import { Spinner } from './components/spinner/Spinner';
 
-const Home = lazy(() => import("./pages/home/HomePage"));
-const GamesRouting = lazy(() => import("./pages/games/GamesRouting"));
-const OptionsRouting = lazy(() => import("./pages/options/OptionsRouting"));
-const ContactPage = lazy(() => import("./pages/contact/ContactPage"));
+const Home = lazy(() => import('./pages/home/HomePage'));
+const GamesRouting = lazy(() => import('./pages/games/GamesRouting'));
+const OptionsRouting = lazy(() => import('./pages/options/OptionsRouting'));
+const ContactPage = lazy(() => import('./pages/contact/ContactPage'));
 
 export function AppRouting() {
     return (
         <Suspense fallback={<Spinner />}>
             <BrowserRouter>
-                <RoutesWithNotFound>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/about" element={<h1>About</h1>} />
-                    <Route path="/contact" element={<ContactPage/>} />
-                    <Route path="/options/*" element={<OptionsRouting />} />
-                    <Route path="/games/*" element={<GamesRouting />} />
-                </RoutesWithNotFound>
+                <DartsContextProvider>
+                    <RoutesWithNotFound>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/about" element={<h1>About</h1>} />
+                        <Route path="/contact" element={<ContactPage />} />
+                        <Route path="/options/*" element={<OptionsRouting />} />
+                        <Route path="/games/*" element={<GamesRouting />} />
+                    </RoutesWithNotFound>
+                </DartsContextProvider>
             </BrowserRouter>
         </Suspense>
     );
