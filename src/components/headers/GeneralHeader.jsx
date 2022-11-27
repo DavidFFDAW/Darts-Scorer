30309268efd7a0d472b8d79eeefd988d9e07ed22
DavidFFDAW <davidferflo2@gmail.com>
2022-11-27 14:10:39 +0100

    feat(headers): new header type with no round

--- src/components/headers/GeneralHeader.jsx
+++ src/components/headers/GeneralHeader.jsx
@@ -1,6 +1,5 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import { Icon } from "../icon/Icon";
+import { Link } from 'react-router-dom';
+import { Icon } from '../icon/Icon';
 
 export function GeneralHeader({ children }) {
     const childrenOrNull = children || null;
@@ -14,22 +13,29 @@ export function GeneralHeader({ children }) {
     );
 }
 
+export function GeneralHeaderWithNoRound({ children }) {
+    const childrenOrNull = children || null;
+    return (
+        <>
+            <div className="general-header flex between">{childrenOrNull}</div>
+        </>
+    );
+}
+
 export function DefaultGeneralHeader({ text, animation = false }) {
-    const animationClass = animation
-        ? "animate__animated animate__pulse animate__fast animate__delay-2s animate__infinite"
-        : "";
+    const animationClass = animation ? 'animate__animated animate__pulse animate__fast animate__delay-2s animate__infinite' : '';
 
     return (
         <>
             <div className="general-header flex between">
-                <Link to={"/"}>
-                    <Icon icon={"home"}></Icon>
+                <Link to={'/'}>
+                    <Icon icon={'home'}></Icon>
                 </Link>
                 <span className={animationClass}>{text}</span>
-                <Link to={"/settings"}>
-                    <Icon icon={"settings"}></Icon>
+                <Link to={'/settings'}>
+                    <Icon icon={'settings'}></Icon>
                 </Link>
-                <div className="general-header round-back"></div>{" "}
+                <div className="general-header round-back"></div>{' '}
             </div>
         </>
     );
