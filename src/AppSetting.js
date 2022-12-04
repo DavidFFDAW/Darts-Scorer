70409aabd304ba992dc35019a712f10ad3ec006f
DavidFFDAW <davidferflo2@gmail.com>
2022-12-04 17:37:21 +0100

    feat(cricket): cricket game added

--- src/AppSetting.js
+++ src/AppSetting.js
@@ -4,7 +4,7 @@ export class AppSettings {
     static MAIL_SUBJECT_SEND = 'Soporte App Dardos';
     static MAIL_API_ENDPOINT = 'https://vps-f87b433e.vps.ovh.net/mail/mail.api.php';
 
-    static AVAILABLE_GAMES = [301, 501, 701, 1001];
+    static AVAILABLE_GAMES = [301, 501, 701, 1001, 'cricket'];
     static AVAILABLE_PLAYERS = [2, 3, 4, 5, 6];
     static AVAILABLE_TYPES = {
         game: 'game',
